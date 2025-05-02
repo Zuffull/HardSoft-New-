5bed76d1dd419183acd1905950252fec91d8ea85
Zuffull <sophiasuroid@gmail.com>
2025-05-02 23:12:38 +0300

    1

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Header from '../components/Header';
-
+import Categories from '../components/Categories';
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Categories />
       <main>
       </main>
     </div>
