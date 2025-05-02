865e0ad2396757ecb3bd4a74d2f6b6ec17630cd2
Zuffull <sophiasuroid@gmail.com>
2025-05-02 21:31:43 +0300

    1

--- src/app/cart/page.js
+++
@@ -1,8 +0,0 @@
-export default function CartPage() {
-    return (
-      <div>
-        <h1>Кошик</h1>
-        <p>Це сторінка з кошиком.</p>
-      </div>
-    );
-  }
--- src/app/favorites/page.js
+++ src/app/favorites/page.js
@@ -1,29 +1,12 @@
-import './favoritespage.css';
-import Link from 'next/link';
+import Header from '@/components/Header';
 
-export default function FavoritesPage() {
-    return (
-      <div className='Header'>
-        <div className='header_line'>
-          <div className='header_logo'>
-            <img src='Logo.png' alt='Logo' />
-          </div>
-          <h1 className='Logo_name'>HardSoft</h1>
-            <div className='adresa'>Україна, м. <br/>Шептицький</div>
-          <p className='Street'>вул. Стуса буд. 3</p>
-          <p className='Street'>Пн.-Пт. з 09:00 до 19:00 <br/>Сб. з 10:00 до 18:00 </p>
-          <div className='button'>
-          <Link href='#'>
-            <img src='Вхід.png' alt='Вхід'/>
-          </Link>
-          <Link href='/favorites'>
-            <img src='Вибране.png' alt='Обране'/>
-          </Link>
-            <Link href='/cart'>
-              <img src='Кошик.png' alt='Кошик'/>
-            </Link>
-          </div>
-        </div>
-      </div>
-    );
-  }
+
+export default function Favorite() {
+  return (
+    <div>
+      <Header />
+      <main>
+      </main>
+    </div>
+  );
+}
